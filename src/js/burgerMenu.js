4311c4b478778a2be9d51b43232861b5325e9d77
vivtorochok <vika17052001@gmail.com>
2026-07-03 13:35:30 +0300

    update header (not all)

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,11 +1,14 @@
 const openBtnEl = document.querySelector('[data-action="open"]');
 const closeBtnEl = document.querySelector('[data-action="close"]');
 const burgerMenuEl = document.querySelector('[data-visible]');
+const headerEl = document.querySelector('[data-el="header"]');
 
-openBtnEl.addEventListener('click', e => {
+openBtnEl.addEventListener('click', () => {
   burgerMenuEl.dataset.visible = 'open';
+  headerEl.dataset.menu = 'open';
 });
 
-closeBtnEl.addEventListener('click', e => {
+closeBtnEl.addEventListener('click', () => {
   burgerMenuEl.dataset.visible = 'close';
+  headerEl.dataset.menu = 'close';
 });
